a7f1675f53294f5f0f1833165c583f4edab31267
GraphhOrlov <seregaorlov.gus@yandex.ru>
2026-07-23 17:51:18 +0300

    add parametrize

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -2,6 +2,14 @@ from src.math_utils import Math_Operations
 
 import pytest
 
-def test_divide():
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        (1, 2, 0.5),
+        (3, 3, 1),
+        (10, 5, 2)
+    ]
+)
+def test_divide(a, b, expected):
     utils = Math_Operations()
-    assert utils.divide(5 ,3)
+    assert utils.divide(a, b) == expected
